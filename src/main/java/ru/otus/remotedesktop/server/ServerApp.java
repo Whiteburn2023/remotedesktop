bdb068a27983578c61a1675645b5f759e987d276
whiteburn <erunda995@yandex.ru>
2026-01-08 14:31:28 +0500

    commit 06 добален logger v ServerApp

--- src/main/java/ru/otus/remotedesktop/server/ServerApp.java
+++ src/main/java/ru/otus/remotedesktop/server/ServerApp.java
@@ -1,10 +1,14 @@
 package ru.otus.remotedesktop.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.awt.*;
 import java.io.IOException;
 import java.net.ServerSocket;
 
 public class ServerApp {
+    private static final Logger logger = LoggerFactory.getLogger(ServerApp.class);
     private static final int DEFAULT_PORT = 5900;
 
     public static void main(String[] args) {
@@ -13,20 +17,25 @@ public class ServerApp {
             try {
                 port = Integer.parseInt(args[0]);
             } catch (NumberFormatException e) {
-                System.out.println("неверный порт" + DEFAULT_PORT);
+                logger.error("неверный формат порта: {}. используется порт по умолчанию: {}", args[0], DEFAULT_PORT, e);    //System.out.println("неверный порт" + DEFAULT_PORT);
             }
         }
 
-        System.out.println("запуск сервера на порту " + port);
+        logger.info("запуск сервера на порту {}", port);    //System.out.println("запуск сервера на порту " + port);
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("сервер запущен. ждем подключения ");
 
             while (true) {
+                logger.debug("ожидание нового подключения");
                 new ConnectionHandler(serverSocket.accept()).start();
+                logger.info("новое подключение принято. соединение");
             }
         } catch (IOException | AWTException e) {
-            System.err.println("ошибка сервера " + e.getMessage());
+            logger.error("критическая ошибка сервера на порту {}: {}", port, e.getMessage(), e);    //System.err.println("ошибка сервера " + e.getMessage());
+        } catch (Exception e) {
+            logger.error("неожиданная ошибка в работе сервера", e);
         }
     }
+
 }
